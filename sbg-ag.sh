ef601489eea441c7755d77ef8f4c052b760ccd50
Georg Schreglmann <georg.schreglmann@vivid-planet.com>
2024-05-13 18:09:59 +0200

    Add Xvfb and nvm to startscript

--- sbg-ag.sh
+++ sbg-ag.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
+Xvfb :99 &
+export DISPLAY=:99
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use 20
+
 ./bin/infra.js download-salzburg-ag --target day --headless
 ./bin/infra.js import-salzburg-ag --target day --file ./Lastprofilwerte.csv
 rm ./Lastprofilwerte.csv
