ec79116c39e3465f109d85b83c49fa1f13ef1597
Georg Schreglmann <georg.schreglmann@vivid-planet.com>
2024-05-20 17:16:50 +0200

    Remove Xvfb and add logs to exec script

--- sbg-ag.sh
+++ sbg-ag.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env sh
-Xvfb :99 &
-export DISPLAY=:99
-
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm use 20
+nvm use
 
 ./bin/infra.js download-salzburg-ag --target day --headless
 ./bin/infra.js import-salzburg-ag --target day --file ./Lastprofilwerte.csv
@@ -12,3 +9,5 @@ rm ./Lastprofilwerte.csv
 ./bin/infra.js download-salzburg-ag --target night --headless
 ./bin/infra.js import-salzburg-ag --target night --file ./Lastprofilwerte.csv
 rm ./Lastprofilwerte.csv
+
+echo $(date '+%Y-%m-%d %H:%M:%S') >> ./last-runs.txt
